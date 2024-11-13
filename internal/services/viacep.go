393c6745423ce1e4c6d4caeaef6aa0929dc86f53
marcosocram <marcosocram@gmail.com>
2024-11-13 04:45:38 -0300

    first-commit

--- internal/services/viacep.go
+++ internal/services/viacep.go
@@ -14,11 +14,24 @@ type ViaCEPResponse struct {
 }
 
 func GetCidadeFromCEP(cep string) (string, error) {
-	resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep), nil)
 	if err != nil {
+		fmt.Println("1: ", err)
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("2: ", err)
 		return "", err
 	}
 	defer resp.Body.Close()
+
+	//resp, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	//if err != nil {
+	//	return "", err
+	//}
+	//defer resp.Body.Close()
 
 	var data ViaCEPResponse
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil || data.Localidade == "" {
